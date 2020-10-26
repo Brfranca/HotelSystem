0ad02b3059532e0d36f09ffd756f799132074a79
Kátia Alexsandra Jung <katiajung@outlook.com>
2020-10-26 16:54:21 -0300

    Alterações em EmployeeDAL e EmployeeBLL

---
+++ BusinessLogicalLayer/BLL/EmployeeBLL.cs
@@ -0,0 +1,191 @@
+﻿using BusinessLogicalLayer.Extentions;
+using Common;
+using DataAccessLayer.DAL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicalLayer.BLL
+{
+    class EmployeeBLL
+    {
+        private readonly EmployeeDAL _employeeDAL;
+
+        public EmployeeBLL()
+        {
+            _employeeDAL = new EmployeeDAL();
+        }
+
+        public Response Register(Employee employee)
+        {
+            Response result = Validate(employee);
+            if (!result.Success)
+                return result;
+
+
+            Response resultInsert = _employeeDAL.Insert(employee);
+            if (!resultInsert.Success)
+                return resultInsert;
+
+            //_employee.Insert(employee) vem do BaseDAL 
+
+            return Response.CreateSuccess("Funcionário cadastrado com sucesso!");
+        }
+
+        public Response Update(Employee employee)
+        {
+            Response result = Validate(employee);
+            if (!result.Success)
+                return result;
+
+            Response resultUpdate = _employeeDAL.Update(employee);
+            if (!resultUpdate.Success)
+                return resultUpdate;
+
+            return Response.CreateSuccess("Funcionário atualizado com sucesso!");
+        }
+
+
+        public Response Delete(Employee employee)
+        {
+            Response resultDelete = _employeeDAL.Delete(employee);
+            if (!resultDelete.Success)
+                return resultDelete;
+
+            return Response.CreateSuccess("Funcionário removido com sucesso!");
+        }
+
+        public QueryResponse<List<Employee>> GetAll()
+        {
+            return _employeeDAL.GetAll();
+        }
+
+        public QueryResponse<Employee> GetById(int id)
+        {
+            return _employeeDAL.GetById(id);
+        }
+
+        public QueryResponse<Employee> GetByCpf(string cpf)
+        {
+            return _employeeDAL.GetByCpf(cpf);
+        }
+
+        public QueryResponse<Employee> GetByRg(string rg)
+        {
+            return _employeeDAL.GetByRg(rg);
+        }
+
+        private Response Validate(Employee employee)
+        {
+            try
+            {
+                Validator validator = new Validator();
+                ValidateEmail(employee.Email, employee.ID, validator);
+                ValidateRg(employee.RG, employee.ID, validator);
+                ValidateCpf(employee.CPF, employee.ID, validator);
+                ValidateName(employee.Name, validator);
+                ValidatePhone(employee.Phone, validator);
+
+                return validator.Validate();
+
+                /*
+                O validator.Validate() verifica se o comprimento da StringBuilder _errors é igual a zero, 
+                caso seja vai retornar Response.CreateSuccess();
+                Caso nao seja retornará Response.CreateFailure(_errors.ToString());
+                */
+
+                //Por que usar o id nos métodos ValidateEmail, ValidateRg e ValidateCpf?????
+
+            }
+            catch (Exception erro)
+            {
+                return Response.CreateFailureException("Erro na validação do cliente!", erro);
+            }
+        }
+
+
+        private void ValidateEmail(string email, int id, Validator validator)
+        {
+            if (email.IsNullOrWhiteSpace())
+            {
+                validator.AddError("Email deve ser informado!");
+            }
+            else if (email.IsValidEmail())
+            {
+                validator.AddError("Email inválido!");
+            }
+            else if (_employeeDAL.ExistEmail(email, id))
+            {
+                validator.AddError("Email já cadastrado em nossa base de dados!");
+            }
+        }
+
+        private static void ValidatePhone(string phone, Validator validator)
+        {
+            if (phone.IsNullOrWhiteSpace())
+            {
+                validator.AddError("Telefone deve ser informado!");
+            }
+            else if (phone.IsValidPhone())
+            {
+                validator.AddError("Telefone inválido!");
+            }
+            
+        }
+
+        private void ValidateRg(string rg, int id, Validator validator)
+        {
+            if (rg.IsNullOrWhiteSpace())
+            {
+                validator.AddError("RG deve ser informado!");
+            }
+            else if (rg.Length < 4)
+            {
+                validator.AddError("RG inválido!");
+            }
+            else if (rg.Any(char.IsLetter))
+            {
+                validator.AddError("RG deve conter apenas números!");
+            }
+            else if (_employeeDAL.ExistRg(rg, id))
+            {
+                validator.AddError("RG já cadastrado em nossa base de dados!");
+            }
+        }
+
+        private void ValidateCpf(string cpf, int id, Validator validator)
+        {
+            if (cpf.IsNullOrWhiteSpace())
+            {
+                validator.AddError("CPF deve ser informado!");
+            }
+            else if (cpf.Length < 11)
+            {
+                validator.AddError("CPF deve conter 11 caracteres!");
+            }
+            else if (cpf.IsValidCPF())
+            {
+                validator.AddError("CPF inválido!");
+            }
+            else if (_employeeDAL.ExistCpf(cpf, id))
+            {
+                validator.AddError("CPF já cadastrado em nossa base de dados!");
+            }
+        }
+
+        private static void ValidateName(string name, Validator validator)
+        {
+            if (name.IsNullOrWhiteSpace())
+            {
+                validator.AddError("Nome deve ser informado!");
+            }
+            else if (name.IsValidName())
+            {
+                validator.AddError("Nome completo deve ser informado!");
+            }
+        }
+    }
+}
--- DataAccessLayer/DAL/EmployeeDAL.cs
+++ DataAccessLayer/DAL/EmployeeDAL.cs
@@ -12,6 +12,31 @@ namespace DataAccessLayer.DAL
 {
     public class EmployeeDAL : BaseDAL<Employee>, IEntityCRUD<Employee>
     {
+        public QueryResponse<Employee> GetByCpf(string cpf)
+        {
+            /*
+            DbFactory.GetCurrentCommand tem como retorno: GetInstance()._provider.CreateCommand()
+            _provider = DbProviderFactories.GetFactory(_providerName);
+            _providerName = ConfigurationManager.ConnectionStrings[currentConnection].ProviderName;
+            ProviderName: string dentro do App.config que está dentro do PresentationLayer 
+            podendo ser "SqlServerConnection", "OracleServerCOnnection"... dependendo de qual banco de dados 
+            está sendo utilizado
+            */
+
+            DbCommand command = DbFactory.GetCurrentCommand();
+            command.CommandText = $"{_select} WHERE CPF = @CPF";
+            command.Parameters.AddWithValue("@CPF", cpf);
+            return new DbExecuter().GetSingleData<Employee>(command);
+        }
+
+        public QueryResponse<Employee> GetByRg(string rg)
+        {
+            DbCommand command = DbFactory.GetCurrentCommand();
+            command.CommandText = $"{_select} WHERE RG = @RG";
+            command.Parameters.AddWithValue("@RG", rg);
+            return new DbExecuter().GetSingleData<Employee>(command);
+        }
+
         public bool ExistCpf(string cpf, int id)
         {
             return Exist(cpf, id, "CPF");
